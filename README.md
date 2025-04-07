015641425cfa0e663cc77664269407bc675c5023
0cr0wbar0 <finnianholland2@gmail.com>
2025-04-07 16:55:29 +0100

    Final Things (Hopefully) Added thumbnails for links to models (Hopefully) finalised the about page

---
+++ README.md
@@ -0,0 +1,3 @@
+# Finn Holland's 3D Web App
+
+### Available online at: https://threedee.cr0wbar.dev
--- index.php
+++ index.php
@@ -9,6 +9,9 @@
         body {
             height: 100vh;
         }
+        img {
+            border-radius: 5px;
+        }
     </style>
 </head>
 
@@ -19,12 +22,16 @@
 </div>
 
 <div class="nav">
-<a href="static/bear_ears.html">Hugh the Grizzly Bear</a><br/>
-<a href="static/cow_ears.html">Brianna the Highland Cow</a><br/>
-<a href="static/blob_hands.html">The Listener</a>
+<a href="static/bear_ears.html"><img src="static/bear_mugshot.png" width="50" height="50" alt="hugh mugshot">Hugh the Grizzly Bear</a><br/>
+<a href="static/cow_ears.html"><img src="static/cow_mugshot.png" width="50" height="50" alt="brianna mugshot">Brianna the Highland Cow</a><br/>
+<a href="static/blob_hands.html"><img src="static/blob_mugshot.png" width="50" height="50" alt="listener mugshot">The Listener</a>
 </div>
 <div class="nav">
     <a href="static/about.html">About this Website</a>
+</div>
+
+<div class="text">
+    <p>&copy; Finnian Holland</p>
 </div>
 
 </body>
